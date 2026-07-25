3a9fa48404dfdec8c4523d6d95f71e2dbb95e047
keshavsoft <nknnkumar@yahoo.com>
2026-07-25 11:54:25 +0530

    KeshavSoft

---
+++ bin/v2/index.js
@@ -0,0 +1,38 @@
+import packageJson from '../../package.json' with {type: 'json'};
+
+const totalLines = ({ fileContent }) => {
+    const fileLines = fileContent.split(/\r?\n/);
+
+    return fileLines;
+};
+
+const totalLinesStory = (allLines) => {
+
+    const allLinesStory = allLines.map((element, loopIndex) => {
+        return {
+            line: element,
+            lineNumber: loopIndex + 1
+        };
+    });
+
+    return allLinesStory;
+};
+
+const startFunc = ({ fileContent, showLog = false }) => {
+
+    if (showLog?.keysOnly) console.log(`${packageJson.name}-start`);
+    if (showLog?.withValues) console.log(`${packageJson.name}-inputs : `, fileContent, parseRegex, searchRegex);
+
+    const allLines = totalLines({ fileContent });
+
+    const allLinesStory = totalLinesStory(allLines);
+
+    if (showLog?.keysOnly) console.log(`${packageJson.name}-end`);
+    if (showLog?.withValues) console.log(`${packageJson.name}-outputs : `, importLines);
+
+    return {
+        allLines
+    };
+};
+
+export default startFunc;
