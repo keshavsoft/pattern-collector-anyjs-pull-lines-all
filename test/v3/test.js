90270a6c51ab32d37e5411aa33cc223e9947712b
keshavsoft <nknnkumar@yahoo.com>
2026-07-28 21:52:53 +0530

    4 no dep

---
+++ test/v3/test.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import defaultFunc from '../../index.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "routes.js");
+
+const fileContent = fs.readFileSync(filePath, 'utf8');
+
+const k1 = defaultFunc({
+    fileContent,
+    showLog: false,
+    showLogStep1: false
+});
+
+console.log("ssssssssss : ", k1);
+
